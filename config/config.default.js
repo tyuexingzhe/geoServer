2979ca90693a9e403d3146c0de14dbc55e205ba4
tyf <448371352@qq.com>
2017-11-20 23:51:00 +0800

    暂时关闭csrf

--- config/config.default.js
+++ config/config.default.js
@@ -19,6 +19,12 @@ module.exports = appInfo => {
     notfound: {
       pageUrl: '/404.html',
     },
+
+    security: {
+      csrf: {
+        ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+      },
+    },
   };
   return config;
 };
